d5c5f4f053037825ff803f6c5d5f25d830f7aa62
Shridhar <spaw0004@student.monash.edu>
2020-05-27 09:19:51 +1000

    Pom change

--- src/test/java/allaboutecm/model/AlbumUnitTest.java
+++ src/test/java/allaboutecm/model/AlbumUnitTest.java
@@ -382,7 +382,6 @@ class AlbumUnitTest {
     public void salesShouldBeValid(int arg){
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,()->album.setSales(arg));
         assertEquals(exception.getMessage(),"Sales number should be greater than or equal to 0.");
-   
     }
 
     @ParameterizedTest
